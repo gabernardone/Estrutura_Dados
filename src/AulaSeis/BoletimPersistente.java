693d1049b956e4daf670bf98f254fa5a96c62bda
unknown <aluno@Dell95-5C1M0Y1.band.net>
2015-09-10 22:06:19 -0300

    Exercicio 11-B

--- src/AulaSeis/BoletimPersistente.java
+++ src/AulaSeis/BoletimPersistente.java
@@ -1,5 +1,45 @@
 package AulaSeis;
 
+import java.util.Scanner;
+
 public class BoletimPersistente {
+
+	private Double MediaAprovacao;
+	private int NumeroFaltas;
+	
+	public void run() {
+		Scanner readLine = new Scanner(System.in);
+
+		System.out.println("Qual a média de aprovação?");
+		this.MediaAprovacao = Double.parseDouble(readLine.nextLine());
+
+		System.out.println("Qual o máximo de faltas?");
+		this.NumeroFaltas = Integer.parseInt(readLine.nextLine());
+
+		this.cadastrarNotasFrequencia(0);
+	}
+
+	private void cadastrarNotasFrequencia( int n ) {
+		Scanner readLine = new Scanner(System.in);
+
+		System.out.println("Qual a nota um?");
+		Double NotaUm = Double.parseDouble(readLine.nextLine());
+		
+		System.out.println("Qual a nota dois?");
+		Double NotaDois = Double.parseDouble(readLine.nextLine());
+		
+		System.out.println("Qual a quantidade de falas?");
+		int Faltas = Integer.parseInt(readLine.nextLine());
+		
+		Double media = ((NotaUm+NotaDois)/2);
+		
+		if( media > this.MediaAprovacao &&  Faltas < this.NumeroFaltas)
+		{
+			System.out.println("Parabéns pela persistência, você tentou "+n+" vezes até passar em estrutura de dados!");
+		} else {
+			System.out.println("Tente novamentes");
+			this.cadastrarNotasFrequencia(n++);
+		}
+	}
 
 }
--- src/AulaSeis/Main.java
+++ src/AulaSeis/Main.java
@@ -7,8 +7,11 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		Randomic rand = new Randomic();
-
-		rand.run(7);
+		//Randomic rand = new Randomic();
+		//rand.run(7);
+		
+		
+		BoletimPersistente boletin = new BoletimPersistente();
+		boletin.run();
 	}
 }
