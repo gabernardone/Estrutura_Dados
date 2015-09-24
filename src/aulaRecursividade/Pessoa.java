76c5dd1396b668b693d9869fab6090466372e579
unknown <aluno@Dell84-E91K0Y1.band.net>
2015-09-24 19:37:20 -0300

    Package aulaRecursividade

---
+++ src/aulaRecursividade/Pessoa.java
@@ -0,0 +1,24 @@
+package aulaRecursividade;
+
+public class Pessoa {
+	private String Nome;
+	private double Altura;
+	
+	public String getNome() {
+		return Nome;
+	}
+	
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	public double getAltura() {
+		return Altura;
+	}
+	
+	public void setAltura(double altura) {
+		Altura = altura;
+	}
+	
+	
+}
---
+++ src/aulaRecursividade/programaPessoas.java
@@ -0,0 +1,16 @@
+package aulaRecursividade;
+
+public class programaPessoas {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Pessoa joao = new Pessoa();
+		
+		joao.setNome("Jõao");
+	}
+
+}
