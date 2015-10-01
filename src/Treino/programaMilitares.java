d35ff61acdc0b9c60e57e72a177da043dc4806ed
unknown <aluno@Dell18-9C1M0Y1.band.net>
2015-10-01 20:23:27 -0300

    Programa Militares (Recursividade de objetos)

--- src/Treino/programaMilitares.java
+++ src/Treino/programaMilitares.java
@@ -13,7 +13,32 @@ public class programaMilitares {
 		MilitarAsp.setNome("André");
 		
 		MilitarGen.setInferior(MilitarSar);
+		MilitarGen.setInferior(MilitarAsp);
+		
+		MilitarSar.setSuperior(MilitarGen);
 		MilitarSar.setInferior(MilitarAsp);
 		
+		MilitarAsp.setSuperior(MilitarSar);
+		
+		System.out.println("1º Militar: "+ MilitarGen.getNome());
+		System.out.println("É chefão: "+ MilitarGen.isGeneral());
+		System.out.println("É aspira: "+ MilitarGen.isAspira());
+		System.out.println("Seu inferior é :" + MilitarGen.getInferior().getNome());
+		System.out.println("--------------------------------------------------------");
+		
+		System.out.println("2º Militar: "+ MilitarSar.getNome());
+		System.out.println("É chefão: "+ MilitarSar.isGeneral());
+		System.out.println("É aspira: "+ MilitarSar.isAspira());
+		System.out.println("Seu inferior é :" + MilitarSar.getInferior().getNome());
+		System.out.println("Seu superior é :" + MilitarSar.getSuperior().getNome());
+		System.out.println("--------------------------------------------------------");
+		
+		System.out.println("3º Militar: "+ MilitarAsp.getNome());
+		System.out.println("É chefão: "+ MilitarAsp.isGeneral());
+		System.out.println("É aspira: "+ MilitarAsp.isAspira());
+		System.out.println("Seu superior é :" + MilitarAsp.getSuperior().getNome());
+		
+		
+		System.out.println("__ \nfim programa");
 	}
 }
